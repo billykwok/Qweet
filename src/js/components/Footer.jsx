ae2088795b5b44e064e761ae64a43d58ba62bb07
Billy Kwok <xobkwok@gmail.com>
2015-10-15 11:01:38 +0800

    add textarea for message

--- src/js/components/Footer.jsx
+++ src/js/components/Footer.jsx
@@ -10,7 +10,7 @@ class Navbar extends React.Component {
     render() {
         return (
             <footer className="bd-footer text-muted">
-                <div className="container">
+                <div className="container text-center">
                     <p>Built with love by <a href="https://fb.com/getideamagnet" target="_blank">Memory Leaker</a> © 2015</p>
                 </div>
             </footer>
--- src/js/components/QuestionForm.jsx
+++ src/js/components/QuestionForm.jsx
@@ -16,14 +16,29 @@ class QuestionForm extends React.Component {
     static getPropsFromStores() { return QuestionStore.getState(); }
     render() {
         return (
-            <form>
-                <fieldset className="form-group">
-                    <label htmlFor="exampleInputEmail1">Message</label>
-                    <input type="text" className="form-control" placeholder="Enter question here..." />
-                </fieldset>
+            <form onSubmit={ this.handleSubmit }>
+                <label className="sr-only">Headline</label>
+                <input ref=""
+                    type="text"
+                    className="form-control form-control-bundle-first"
+                    placeholder="Headline" />
+                <label className="sr-only">Message</label>
+                <textarea ref=""
+                    className="form-control form-control-bundle-last"
+                    placeholder="Message"
+                    rows={4} />
                 <button type="submit" className="btn btn-primary">Post</button>
             </form>
         );
+    }
+    handleSubmit(e) {
+        e.preventDefault();
+        const author = this.refs.author.value.trim();
+        const text = this.refs.text.value.trim();
+        if (!text || !author) { return; }
+        this.refs.author.value = "";
+        this.refs.text.value = "";
+        return;
     }
 }
 
